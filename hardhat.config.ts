b0aec40da3c5186f2a583f177dcd14d883580cfb
Nischal Shakya <nischalshakya2015@gmail.com>
2022-09-30 23:27:25 +0545

    add goerli testnet deploy config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: process.env.GOERLI_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY!],
+            chainId: 5,
+        },
+    },
+    solidity: "0.8.17",
 };
 
 export default config;
